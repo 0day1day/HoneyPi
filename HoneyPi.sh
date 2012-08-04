97e97a00ae4de1db7e8018cc03e85bc65ed588d1
Steeve BARBEAU <steeve.barbeau@gmail.com>
2012-08-04 09:18:59 +0200

    minor fixes on HoneyPi.sh

--- HoneyPi.sh
+++ HoneyPi.sh
@@ -19,12 +19,18 @@ do
     echo "########################" >> $OUTPUT_FILE
     echo "$f ..." >> $OUTPUT_FILE
     echo "    KIPPO stats ..." >> $OUTPUT_FILE
-    ./kippo.py "$HONEYPI_DIR/$f/kippo-0.5/" "./ext/GeoIP.dat" >> $OUTPUT_FILE
+    ./kippo.py "$HONEYPI_DIR/$f/kippo-0.5/" "./ext/GeoIP.dat" &>> $OUTPUT_FILE
 
     echo -e '\n\n' >> $OUTPUT_FILE
+    echo "    BINARIES stats ..." >> $OUTPUT_FILE
+    ./VirusTotalUploader.py -d 1 -y -r "$HONEYPI_DIR/$f/dionaea/binaries/" &>> $OUTPUT_FILE
+    echo -e '\n\n' >> $OUTPUT_FILE
+
+
     echo "    DIONAEA stats ..." >> $OUTPUT_FILE
-    ./VirusTotalUploader.py -d 1 -y -r "$HONEYPI_DIR/$f/dionaea/binaries/" >> $OUTPUT_FILE
+    ./Dionaea.py "$HONEYPI_DIR/$f/dionaea/logsql.sqlite" "./ext/GeoIP.dat" &>> $OUTPUT_FILE
     echo -e '\n\n\n\n' >> $OUTPUT_FILE
+
   fi
 done
 
